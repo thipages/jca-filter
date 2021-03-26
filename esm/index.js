7a4f9e683276d8a1c3fe361ecf50645e777aef06
thipages <thipages@gmail.com>
2021-03-26 10:49:42 +0100

    updates

--- esm/index.js
+++ esm/index.js
@@ -9,10 +9,14 @@ export default (logic,otherConditions={})=> {
     });
     const castArray=a=>Array.isArray(a)?a:[a];
     const postFilter=i=>i===0?'':i;
-    return Object.assign(otherConditions,parseBooleanQuery(re).reduce(
-        (acc,v,i)=>{
-            acc['filter'+postFilter(i)]=castArray(v).map(v=>stack[v]);
-            return acc;
-        },{}
-    )); 
+    const parsed=parseBooleanQuery(re);
+    
+    return parsed
+        ? Object.assign(otherConditions,parsed.reduce(
+            (acc,v,i)=>{
+                acc['filter'+postFilter(i)]=castArray(v).map(v=>stack[v]);
+                return acc;
+            },{}
+        ))
+        :{}; 
 }
