37d02a490ebec34a0e2eea70d1a5f0dad6c33802
thipages <thipages@gmail.com>
2021-06-21 08:16:59 +0200

    add copy button

--- site/index.js
+++ site/index.js
@@ -3,6 +3,22 @@ import jca_filter from  './../esm/index.js';
 example.addEventListener(
     'click',(e)=>content.value="('field1,eq,val1' OR 'field2,eq,val2') AND 'field3,eq,val3'"
 );
+const validateCopy=(valid)=>{
+  const back=copy.style.background;
+  copy.style.background=valid?'lightgreen':'red';
+  setTimeout(()=>copy.style.background=back,200);
+}
+copy.addEventListener('click',
+  ()=> {
+    var text= generated.innerText;
+    navigator.clipboard.writeText(text).then(function() {
+      validateCopy(true);
+    }, function() {
+      validateCopy(false);
+    });
+
+  }
+);
 const bold=t=>`<span style="font-weight:bold">${t}</span>`;
 generate.addEventListener(
     'click',(e)=>{
@@ -13,6 +29,7 @@ generate.addEventListener(
                 ([k,v])=>v.map(v=>bold(k+'=')+v).join('&')
             ).join('&');
             generated.innerHTML=mm;
+            container.style.display='flex';
         }
     }
 )
